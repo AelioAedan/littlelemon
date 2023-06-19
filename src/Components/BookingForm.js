ab1f1facc3dac373a86906715e0b6b758af463fc
Jonas Baumann <117741411+AelioAedan@users.noreply.github.com>
2023-06-19 10:19:54 +0200

    further adjustments on form

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -36,36 +36,37 @@ const handleSubmit = (e) => {
 
   return (
     <Container pt={200}>
-      <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }} onSubmit={handleSubmit}>
-        <label htmlFor='res-date'>Choose date</label>
-        <input type='date' id='res-date' onChange={changeDate} value={date} required />
-        <label htmlFor='res-time'>Choose time</label>
-        <select id='res-time' value={selectedTime} onChange={changeSelectedTime} required>
-          {availableTimes.map((time) => (
-            <option key={time} value={time}>
-              {time}
-            </option>
-          ))}
-        </select>
-        <label htmlFor='guests'>Number of guests</label>
-        <input
-          type='number'
-          placeholder='1'
-          min='1'
-          max='10'
-          id='guests'
-          value={number}
-          onChange={changeNumber}
-          required
-        />
-        <label htmlFor='occasion'>Occasion</label>
-        <select id='occasion' value={occasion} onChange={changeOccasion} required>
-          <option value='birthday'>Birthday</option>
-          <option value='anniversary'>Anniversary</option>
-        </select>
-        <input type='submit' value='Make Your reservation' disabled={!isEnabled()} />
-      </form>
-    </Container>
+  <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }} onSubmit={handleSubmit}>
+    <label htmlFor='res-date' aria-label='Choose date'>Choose date</label>
+    <input type='date' id='res-date' onChange={changeDate} value={date} required />
+    <label htmlFor='res-time' aria-label='Choose time'>Choose time</label>
+    <select id='res-time' value={selectedTime} onChange={changeSelectedTime} required>
+      {availableTimes.map((time) => (
+        <option key={time} value={time}>
+          {time}
+        </option>
+      ))}
+    </select>
+    <label htmlFor='guests' aria-label='Number of guests'>Number of guests</label>
+    <input
+      type='number'
+      placeholder='1'
+      min='1'
+      max='10'
+      id='guests'
+      value={number}
+      onChange={changeNumber}
+      required
+    />
+    <label htmlFor='occasion' aria-label='Occasion'>Occasion</label>
+    <select id='occasion' value={occasion} onChange={changeOccasion} required>
+      <option value='birthday'>Birthday</option>
+      <option value='anniversary'>Anniversary</option>
+    </select>
+    <input type='submit' value='Make Your reservation' disabled={!isEnabled()} aria-label='On Click' />
+  </form>
+</Container>
+
   );
 }
 
